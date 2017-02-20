9f99b24eee8ebe1af91b9668e25eda2645cd9cd3
Andrew School <andrewschool@Andrews-MacBook-Pro.local>
2017-02-19 16:58:49 -0800

    clean up

--- src/algorithms/grover.py
+++ src/algorithms/grover.py
@@ -6,60 +6,82 @@ from collections import Counter
 
 """
         GROVER'S ALGORITHM
+        by Andrew Gleeson
 
-            Grover's algorithm is a quantum algorithm which probabilistically finds an element in list given a condition
-        function which returns 1 if the element matches the condition and 0 otherwise. Let the list have 2^n
-        elements for some n, meaning each element has a unique n-bit ID. Grover's algorithm works be encoding this
-        condition function, also known as the oracle, into a unitary transformation which has the following properties:
+        Grover's algorithm is a quantum algorithm which probabilistically finds an element in list given a condition
+        function which returns 1 if the element matches the condition and 0 otherwise. Let the list have 2^n elements
+        for some n, meaning each element has a unique n-bit ID. Grover's algorithm has two main elements: the oracle
+        transformation, and inverting about the mean.
 
-            1. If x meets the condition, ORACLE(x) = -x
-            2. If x does not meet the condition, ORACLE(x) = x
+        The oracle is a unitary transformation which has the following properties, given some condition function C(x):
+
+            1. If C(x) = true, ORACLE(x) = -x
+            2. If C(x) = false, ORACLE(x) = x
 
         We can represent this as the identity matrix, where the 1 in the same row as the solution has been flipped to -1.
-        
+        For example, if the solution is the first item in a list of 4 items, then:
+
+            ORACLE = [[-1, 0, 0, 0],
+                      [0, 1, 0, 0],
+                      [0, 0, 1, 0],
+                      [0, 0, 0, 1]]
+
+        The next part of the algorithm is the transformation which inverts about the mean, which is also called the
+        Grover diffusion operator. Given a list of numbers, this operation calculates the average value of the list,
+        and then reflects each value across it. For example, if the average value is m, then:
+
+            INVERT(x) = m + (m - x) = 2m - x.
+
+        Given a uniform superposition state |s>, the diffusion operator can be represented by:
+
+            INVERT = 2|s><s| - I
+
+        Grover's algorithm works by repeatedly applying the oracle and inversion operators on a uniform input state,
+        which causes the correct index to be inverted by the oracle, and then inverting about the mean makes that
+        index more likely to be measured.
+
+        Interestingly, the probability of measuring the correct index is periodic - it first peaks at sqrt(N)/2,
+        where N is the number of qubits.
+
 
 """
 
-def grover(qvm, num_qubits, sol_index):
-    p = pq.Program()
 
-    p.defgate("invert-about-mean", build_invert_about_mean(num_qubits))
-    p.defgate("oracle", build_oracle(num_qubits, sol_index))
+def grover(num_qubits, sol_index):
+    program = pq.Program()
 
-    diffusion_inst = tuple(["invert-about-mean"] + [i for i in range(num_qubits)])
-    oracle_inst = tuple(["oracle"] + [i for i in range(num_qubits + 1)])
+    # In PyQuil, we can define custom gates like this
+    program += build_invert_about_mean(num_qubits)
+    program += build_oracle(num_qubits, sol_index)
 
-    steps = int(np.round(np.sqrt(num_qubits)/2))
+    diffusion_inst = tuple(["INVERT"] + [i for i in range(num_qubits)])
+    oracle_inst = tuple(["ORACLE"] + [i for i in range(num_qubits + 1)])
+
+    # the probability of success is greatest at iteration sqrt(N)/2
+    num_steps = int(np.round(np.sqrt(num_qubits) / 2))
 
     # initialize
-    for qubit in range(num_qubits+1):
-        p.inst(X(qubit), X(qubit))
-
-    print_program_state("Initial state:", qvm, p, num_qubits)
+    for qubit in range(num_qubits):
+        program.inst(X(qubit), X(qubit))
 
     # Set ancillary bit to |1>
-    p.inst(X(num_qubits))
-
-    print_program_state("Flip ancillary bit:", qvm, p, num_qubits)
+    program.inst(X(num_qubits))
 
     # Create uniform input state
     for i in range(num_qubits + 1):
-        p.inst(H(i))
+        program.inst(H(i))
 
-    print_program_state("Hadamard:", qvm, p, num_qubits)
+    # Repeatedly apply Grover iteration
+    for step in range(num_steps):
+        program.inst(oracle_inst)
+        program.inst(diffusion_inst)
 
-    # Grover's Algorithm
-    for step in range(steps):
-        p.inst(oracle_inst)
-        p.inst(diffusion_inst)
-        print_program_state("After step %d:" % (step + 1), qvm, p, num_qubits)
+    # Measure qubits
+    for i in range(num_qubits):
+        program.measure(i, i)
 
+    return program
 
-    # Create uniform input state
-    for i in range(num_qubits+1):
-        p.measure(i, i)
-
-    return p
 
 def build_invert_about_mean(num_qubits):
     """ Builds the diffusion operator
@@ -78,55 +100,51 @@ def build_invert_about_mean(num_qubits):
 
     # Hadamard gate on two qubits
     hadamard = hadamard_1
-    for _ in range(num_qubits-1):
+    for _ in range(num_qubits - 1):
         hadamard = np.kron(hadamard, hadamard_1)
 
-    return hadamard.dot(invert).dot(hadamard)
+    invert_about_mean_matrix = hadamard.dot(invert).dot(hadamard)
+
+    invert_about_mean = pq.Program()
+    invert_about_mean.defgate("INVERT", invert_about_mean_matrix)
+    return invert_about_mean
+
 
 def build_oracle(num_qubits, sol_index):
-    """ Builds the oracle
+    """ Builds the oracle gate
     """
+    state_size = pow(2, num_qubits + 1)
+    diagonal_entries = [1 for _ in range(state_size)]
+    # bit hackery for correctness (not sure why but otherwise it breaks)
+    reversed_index = int(bin(sol_index)[2:].zfill(num_qubits + 1)[::-1], 2)
+    diagonal_entries[reversed_index] = -1
 
-    # add 1 to account for ancillary qubit
-    dim = pow(2, num_qubits + 1)
-
-    arr = [1 for _ in range(dim)]
-
-    reversed = int(bin(sol_index)[2:].zfill(num_qubits + 1)[::-1], 2)
-
-    arr[reversed] = -1
-
-    return np.diag(arr)
+    oracle = pq.Program()
+    oracle.defgate("ORACLE", np.diag(diagonal_entries))
+    return oracle
 
 
 # UTIL
+def main():
+    qvm = forest.Connection()
+
+    num_qubits = 2
+    sol_index = 0
+
+    p = grover(num_qubits, sol_index)
+
+    print p
+
+    output = qvm.run(p, [0, 1, 2, 3], 20)
+
+    print most_common(output)
+
 
 def most_common(lst):
     lst = map(lambda x: tuple(x), lst)
     data = Counter(lst)
     return data.most_common(1)[0][0]
 
-def main():
-    qvm = forest.Connection()
-
-    num_qubits = 4
-    sol_index = 1
-
-    p = grover(qvm, num_qubits, sol_index)
-
-    print ("Measured: ")
-
-    output = qvm.run(p, [0,1,2,3], 20)
-
-    print (most_common(output))
-
-
-
-def print_program_state(title, qvm, p, num_qubits):
-    print("\n" + title)
-    psi = qvm.wavefunction(p)[0]
-    for i in range(len(psi)):
-        print "%s: %.3f" % (bin(i)[2:].zfill(num_qubits + 1), pow(np.real(psi[i]), 2))
 
 if __name__ == '__main__':
     main()
